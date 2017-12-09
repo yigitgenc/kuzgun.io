3c6ba6205fb05960d215b9fe9fea9e1a0f2635c5
Yigit Genc <yigidix@gmail.com>
2017-12-09 03:05:38 +0300

    Include docs URL.

--- src/kuzgun/urls.py
+++ src/kuzgun/urls.py
@@ -7,12 +7,14 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.documentation import include_docs_urls
 
 from .views import HomeView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/docs/', include_docs_urls(title='Kuzgun.io API', description='Dockerized online streaming software.')),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/auth/token', obtain_auth_token),
     url(r'^api/users/', include('users.urls', namespace='users')),
